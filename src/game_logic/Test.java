1b01e25c632a02680889c6d4c508673e5bf9a83f
ptamas98 <71520923+ptamas98@users.noreply.github.com>
2021-03-20 21:57:39 +0100

    Create Test.java

---
+++ src/game_logic/Test.java
@@ -0,0 +1,30 @@
+package game_logic;
+
+public class Test {
+	
+	public void MoveTest()
+	{
+		//TODO
+	}
+	
+	public void DrillTest()
+	{
+		//TODO
+	}
+	
+	public void MineTest()
+	{
+		//TODO
+	}
+	
+	public void CraftRobotTest()
+	{
+		//TODO
+	}
+	
+	public void CraftTeleportTest()
+	{
+		//TODO
+	}
+
+}
