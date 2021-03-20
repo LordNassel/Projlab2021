0d2e9aaf4cfa8f281522209919e516926430d3ef
TakacsArtur <71440410+TakacsArtur@users.noreply.github.com>
2021-03-20 07:27:12 +0100

    Patchwork to better work with Movable.java

--- src/game_logic/Field.java
+++ src/game_logic/Field.java
@@ -1,7 +1,7 @@
 package game_logic;
 
 import java.util.Vector;
-import game_logic.Movable;
+
 
 
 abstract class Field {
@@ -11,25 +11,33 @@ protected Vector<Movable> MovableList = new Vector<Movable>();
 
 //Explode. Kills all things on map then removes itself from the map.
 protected void Explode() {
-	for(int i=0; i<MovableList.Size(); i++)
-		MovableList.Movable.Die();
+	for(int i=0; i<MovableList.size(); i++)
+		MovableList.get(i).Die();
 	Neighbors =null;
 	//delete me from the map pls!!!
 	}
 
 //Sunstorm on a generic asteroid type, the asteroid remains undamaged, all movables die.
 public void SunStorm() {
-		for(int i=0; i<MovableList.Size(); i++)
-			MovableList.Movable.Die();
+		for(int i=0; i<MovableList.size(); i++)
+			MovableList.get(i).Die();
 }
 
 //Simple getter function for all neighbors, used mostly for player and AI navigation 
-public Vector<Field> Getneighbors(){return this.Neighbors;}
+public Vector<Field> FindNeighbor(){return this.Neighbors;}
+
+
 //Simple neighbor setter functon
-public Field SetNeighbor(Field WhichField) {Neighbors.add(WhichField);}
+public void SetNeighbor(Field WhichField) {Neighbors.add(WhichField);}
 
 //To be implemented
 protected void BroadCastRadiation() {};
 
+public void AcceptPlayer (Movable M) {
+	//tbi
+	}
+public void RemovePlayer(Movable M) {
+	MovableList.remove(M);
+}
 
 }
