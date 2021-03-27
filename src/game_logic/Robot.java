bdf706b97b9d210362533dd963a65ef3514f055e
nagytiboradam <nagytiboradam@gmail.com>
2021-03-27 11:40:31 +0100

    MoveToRandomNeighbor, Step

--- src/game_logic/Robot.java
+++ src/game_logic/Robot.java
@@ -1,14 +1,22 @@
 package game_logic;
 
+import java.util.Random;
 import java.util.Vector;
 
-// Represents the robot in te game
+// Represents the robot in the game
 public class Robot extends Movable {
 
 	public Robot(Asteroid position) {
 		super(position);
 	}
 	
+	// A robot atmegy egy random szomszedos aszteroidara
+	private void MoveToRandomNeighbor() {
+		Random rand = new Random();
+		Vector<Field> neighbors = currentField.FindNeighbor();
+	    Field randomNeighbor = neighbors.get(rand.nextInt(neighbors.size()));
+	    Move(randomNeighbor);
+	}
   
 	@Override // A robot felrobbanas overrideja
 	public void HitByExplosion() {
@@ -17,9 +25,15 @@ public class Robot extends Movable {
 		if(neighbors.isEmpty()) {
 			this.Die();
 		} else {
-			this.Move(neighbors.get(0));
+			MoveToRandomNeighbor();
 		}
-		
     }
 
+	@Override // Step funkcio overrideja
+    public void Step() {
+		if(currentField.GetDrilled() == false) {
+			MoveToRandomNeighbor();
+		}
+    }
+	
 }
