d0539b33ea3b53c39a9ad7d92fb7319084a9f9ab
nagytiboradam <nagytiboradam@gmail.com>
2021-04-19 12:20:47 +0200

    Robot comment

--- src/game_logic/Robot.java
+++ src/game_logic/Robot.java
@@ -3,41 +3,50 @@ package game_logic;
 import java.util.Random;
 import java.util.Vector;
 
-// Represents the robot in the game
+/**
+ * Egy mesterséges intelligencia által irányított Robot-ot reprezentál az aszteroidaövben.
+ */
 public class Robot extends Movable {
+	/**
+	 * Random szám generáláshoz..
+	 */
 	Random rand = new Random();
 	
-	//private boolean isDrilled = false;
-
+	/**
+	 * Konstruktor nev nelkul.
+	 */
 	public Robot(Asteroid position) {
 		super(position);
 	}
-	
-	//konstruktor nevvel
+
+	/**
+	 * Konstruktor nevvel.
+	 */
 	public Robot(String name, Asteroid position) {
 		super(position);
 		movablesName = name;
 	}
 	
-	// A robot atmegy egy random szomszedos aszteroidara, ha tud
-	// egyebkent meghal.
-	private void MoveOrDie() {
-		Vector<Field> neighbors = currentField.FindNeighbor(); //Kéne szûrni, hogy csak aszteroida legyen?
+	/**
+	 * Robot-ként eléri a robbanás, aminek hatására kap egy szomszédos aszteroidát a FindNeighbor() függvény segítségével
+	 * és oda “lép” át (AcceptPlayer()) majd lelépteti magát a RemovePlayer() hívással.
+	 */
+	@Override
+	public void HitByExplosion() {
+		Vector<Field> neighbors = currentField.FindNeighbor();
 		if(neighbors.isEmpty()) {
 			Die();
 		} else {
-		    Field randomNeighbor = neighbors.get(rand.nextInt(neighbors.size()));
-		    Move(randomNeighbor);
+			Field randomNeighbor = neighbors.get(rand.nextInt(neighbors.size()));
+			Move(randomNeighbor);
 		}
-	}
-  
-	@Override // A robot felrobbanas overrideja
-	public void HitByExplosion() {
-		MoveOrDie();
     }
-	
-	
-	
+
+
+	/**
+	 *  A robot egy egységnyivel mélyíti az aszteroida köpenyébe fúrt lyukat, ha az aszteroida
+	 *már teljesen át volt fúrva, akkor egy random szomszédos aszteroidára megy át.
+	 */
 	@Override
     public void Step() {
 		if(((Asteroid)currentField).getThickness() == 0) {
